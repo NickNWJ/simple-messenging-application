6c0ac4151af0c53eecf563a7b2f1bc9f050f4be5
NickNWJ <uarehup@gmail.com>
2022-04-10 20:38:27 +0800

    deploy to heroku

--- app/db.conn.php
+++ app/db.conn.php
@@ -1,20 +1,33 @@
 <?php 
 
-# server name
-$sName = "localhost";
-# user name
-$uName = "root";
-# password
-$pass = "";
+// localhost
+// # server name
+// $sName = "localhost";
+// # user name
+// $uName = "root";
+// # password
+// $pass = "";
 
-# database name
-$db_name = "chat_app_db";
+// # database name
+// $db_name = "chat_app_db";
 
-#creating database connection
-try {
-    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
-                    $uName, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-  echo "Connection failed : ". $e->getMessage();
-}
+// #creating database connection
+// try {
+//     $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
+//                     $uName, $pass);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// }catch(PDOException $e){
+//   echo "Connection failed : ". $e->getMessage();
+// }
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+?>
