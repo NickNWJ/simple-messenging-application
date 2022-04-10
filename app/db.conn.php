e93b2a68b5d4848e14cdb758d95c4776bada5de2
NickNWJ <uarehup@gmail.com>
2022-04-11 00:22:51 +0800

    Changes on config

--- app/db.conn.php
+++ app/db.conn.php
@@ -26,8 +26,11 @@ $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
+
 $active_group = 'default';
 $query_builder = TRUE;
+
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+echo("Connection established!");
 ?>
