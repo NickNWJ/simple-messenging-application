9b61a9dde5efb33f3e14ebcba5da4c16310ee06d
NickNWJ <uarehup@gmail.com>
2022-04-11 11:59:31 +0800

    Database configuration

--- app/db.conn.php
+++ app/db.conn.php
@@ -1,12 +1,23 @@
 <?php 
 
-//Get Heroku ClearDB connection information
-$cleardb_server = "us-cdbr-east-05.cleardb.net";
-$cleardb_username = "b9700b4d479412";
-$cleardb_password = "d8b2b505";
-$cleardb_db = "heroku_26135876c73b685";
+// localhost
+# server name
+$sName = "us-cdbr-east-05.cleardb.net";
+# user name
+$uName = "b9700b4d479412";
+# password
+$pass = "d8b2b505";
 
-// Connect to DB
-$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+# database name
+$db_name = "heroku_26135876c73b685";
+
+#creating database connection
+try {
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
+                    $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
 
 ?>
