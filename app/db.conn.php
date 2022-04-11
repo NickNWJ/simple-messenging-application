c7fae0b6ad1d5fb27c6e7c06f5ab85689af64acb
NickNWJ <uarehup@gmail.com>
2022-04-11 11:43:32 +0800

    Database reconfiguration

--- app/db.conn.php
+++ app/db.conn.php
@@ -1,42 +1,11 @@
 <?php 
 
-// localhost
-// # server name
-// $sName = "localhost";
-// # user name
-// $uName = "root";
-// # password
-// $pass = "";
-
-// # database name
-// $db_name = "chat_app_db";
-
-// #creating database connection
-// try {
-//     $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
-//                     $uName, $pass);
-//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-// }catch(PDOException $e){
-//   echo "Connection failed : ". $e->getMessage();
-// }
-
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-
-$active_group = 'default';
-$query_builder = TRUE;
+$cleardb_server = "us-cdbr-east-05.cleardb.net";
+$cleardb_username = "b9700b4d479412";
+$cleardb_password = "d8b2b505";
+$cleardb_db = "heroku_26135876c73b685";
 
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-if ($conn)
-{
-    echo($cleardb_url);
-} else {
-    echo("Connection failed!");
-}
-
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password);
 ?>
